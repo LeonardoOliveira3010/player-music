f68e38b9afa7f0b6cdd9a3a8356af702808eaf85
LeonardoOliveira3010 <65436800+LeonardoOliveira3010@users.noreply.github.com>
2021-08-27 13:58:27 -0300

    Ajuste em algumas funcionalidades e a criação da função de repasse de musica

--- public/script/index.js
+++ public/script/index.js
@@ -1,122 +1,146 @@
 // Objeto Player que conterá todas as informações
 
 const Player = {
-    // Musicas
-        data: [
-            { titulo: "Broken Arrows", artista: 'Avicii', file: '/public/assets/musicas/57-Broken Arrows.mp3', img: '/public/assets/img/capa1.jpg'},
-            { titulo: "Dust in the wind", artista: 'Scorpions', file: '/public/assets/musicas/Dust In The Wind.mp3', img: '/public/assets/img/capa6.jpg'},
-            { titulo: "Take me to church", artista: 'Hozier', file: '/public/assets/musicas/Take Me To Church.mp3', img: "/public/assets/img/capa2.jpg"},
-            { titulo: "another break", artista: 'pink floyd', file: '/public/assets/musicas/another brink in the wall.mp3', img: "/public/assets/img/capa3.jpg"},
-            { titulo: "Stand up", artista: 'Cynthia', file: '/public/assets/musicas/Stand Up.mp3', img:'/public/assets/img/capa5.jpg'},
-            { titulo: "Far Away", artista: 'Nickelback', file: '/public/assets/musicas/Far Away.mp3', img:'/public/assets/img/capa4.jpg'},
-            { titulo: "Photograh", artista: 'Nickelback', file: '/public/assets/musicas/Nickelback - Photograph.mp3', img:'/public/assets/img/capa5.jpg'},
-            { titulo: "oasis", artista: 'Oasis', file: '/public/assets/musicas/oasis.mp3', img:'/public/assets/img/capa7.jpg'},
-            { titulo: "never gonna be alone", artista: 'Nickelback', file: '/public/assets/musicas/never gonna be alone.mp3', img:'/public/assets/img/capa5.jpg'}
-        ],
-    
-        // Variavel audio
-        indexMusica: 0,
-        musica: document.querySelector('audio'),
-    
-        renderizarMusica(index){
-            this.musica.setAttribute('src', this.data[index].file)
-            this.musica.addEventListener('loadeddata', () =>{
-                this.tituloMusica.textContent = this.data[index].titulo
-                this.artista.textContent = this.data[index].artista
-                this.capa.src = this.data[index].img
-                this.fim.textContent = this.tempo(Math.floor(this.musica.duration))
-            })
-    
-            // this.data.forEach(element => console.log(element))
-        },
-    
-    // Variaveis artistas
-        capa: document.querySelector('.capa'),
-        tituloMusica: document.querySelector('.titulo__musica'),
-        artista: document.querySelector('.artista'),
-        
-    // Variaveis do player
-        anterior: document.querySelector('.anterior'),
-        play: document.querySelector('.play'),
-        pause: document.querySelector('.pause'),
-        proximo: document.querySelector('.proximo'),
-    
-    // Funções
-    
-        playMusica(){
-            this.play.addEventListener('click', () =>{
-                // Pegando a musica do array    
-                this.musica.play()
-    
-                this.play.style.display="none"
-                this.pause.style.display="flex"
-    
-            })
-        },
-    
-        pauseMusica(){
-            this.pause.addEventListener('click', () =>{
-                this.musica.pause()
-    
-                this.play.style.display="flex"
-                this.pause.style.display="none"
-    
-            })
-        },
-    
-        musicaAnterior(){
-            this.anterior.addEventListener('click', () =>{
-                this.indexMusica--
-                if(this.indexMusica < 0){
-                    this.indexMusica = this.data.length - 1
-                }
-                this.renderizarMusica(this.indexMusica)
-                this.musica.play()
-                this.play.style.display="none"
-                this.pause.style.display="flex"
-            })
-        },
-    
-        proximaMusica(){
-            this.proximo.addEventListener('click', () =>{
-                this.indexMusica++
-                if(this.indexMusica > this.data.length - 1){
-                    this.indexMusica = 0
-                }
-                this.renderizarMusica(this.indexMusica)
-                
-                this.musica.play()
-                this.play.style.display="none"
-                this.pause.style.display="flex"
-            })
-        },
-    
-    // Variavel referente ao tempo da música    
-        barra: document.querySelector('progress'),
-        inicio: document.querySelector('.inicio'),
-        fim: document.querySelector('.fim'),
-    
-        atualizarBarra(){
-            this.barra.style.width = Math.floor((this.musica.currentTime / this.musica.duration) * 100) + '%'
-            this.inicio.textContent = this.tempo(Math.floor(this.musica.currentTime))
-    
-        },
-    
-        tempo(segundos){
-            let minutos = Math.floor(segundos / 60)
-            let segundo = segundos % 60
-            if(segundo < 10) {
-                segundo = '0' + segundo
+// Musicas
+    data: [
+        { titulo: "Broken Arrows", artista: 'Avicii', file: '/public/assets/musicas/57-Broken Arrows.mp3', img: '/public/assets/img/capa1.jpg'},
+        { titulo: "Dust in the wind", artista: 'Scorpions', file: '/public/assets/musicas/Dust In The Wind.mp3', img: '/public/assets/img/capa6.jpg'},
+        { titulo: "Take me to church", artista: 'Hozier', file: '/public/assets/musicas/Take Me To Church.mp3', img: "/public/assets/img/capa2.jpg"},
+        { titulo: "another break", artista: 'pink floyd', file: '/public/assets/musicas/another brink in the wall.mp3', img: "/public/assets/img/capa3.jpg"},
+        { titulo: "Stand up", artista: 'Cynthia', file: '/public/assets/musicas/Stand Up.mp3', img:'/public/assets/img/capa5.jpg'},
+        { titulo: "Far Away", artista: 'Nickelback', file: '/public/assets/musicas/Far Away.mp3', img:'/public/assets/img/capa4.jpg'},
+        { titulo: "Photograh", artista: 'Nickelback', file: '/public/assets/musicas/Nickelback - Photograph.mp3', img:'/public/assets/img/capa5.jpg'},
+        { titulo: "oasis", artista: 'Oasis', file: '/public/assets/musicas/oasis.mp3', img:'/public/assets/img/capa7.jpg'},
+        { titulo: "never gonna be alone", artista: 'Nickelback', file: '/public/assets/musicas/never gonna be alone.mp3', img:'/public/assets/img/capa5.jpg'}
+    ],
+
+// Variavel audio
+    indexMusica: 0,
+    musica: document.querySelector('audio'),
+
+    renderizarMusica(index){
+        this.musica.setAttribute('src', this.data[index].file)
+        this.musica.addEventListener('loadeddata', () =>{
+            this.tituloMusica.textContent = this.data[index].titulo
+            this.artista.textContent = this.data[index].artista
+            this.capa.src = this.data[index].img
+            this.fim.textContent = this.tempo(Math.floor(this.musica.duration))
+        })
+    // this.data.forEach(element => console.log(element))
+    },
+
+    restartMusica(){
+        this.musica.addEventListener('ended', () =>{
+            this.indexMusica++
+            if(this.indexMusica > this.data.length - 1){
+                this.indexMusica = 0
             }
-            return minutos + ':' + segundo
-        } 
+            this.renderizarMusica(this.indexMusica)
+            this.musica.play()
+        })
+    },
     
-    }
+// Variaveis artistas
+    capa: document.querySelector('.capa'),
+    tituloMusica: document.querySelector('.titulo__musica'),
+    artista: document.querySelector('.artista'),
     
-    // Player.renderizarMusica(indexMusica)
-    Player.playMusica()
-    Player.pauseMusica()
-    Player.proximaMusica()
-    Player.musicaAnterior()
-    Player.atualizarBarra()
-    Player.tempo()
+// Variaveis do player
+    anterior: document.querySelector('.anterior'),
+    play: document.querySelector('.play'),
+    pause: document.querySelector('.pause'),
+    proximo: document.querySelector('.proximo'),
+    
+// Funções
+
+    // startMusica(index){
+    //     this.musica.setAttribute('src', this.data[index].file)
+    //     this.musica.src = this.data[0]
+        
+    //     this.musica.addEventListener('ended', () =>{
+    //         this.tituloMusica.textContent = this.data[index].titulo
+    //         this.artista.textContent = this.data[index].artista
+    //         this.capa.src = this.data[index].img
+    //         this.musica.play()
+    //     })
+    // },
+
+    playMusica(){
+        this.play.addEventListener('click', () =>{
+            // Pegando a musica do array    
+            this.musica.play()
+
+            this.play.style.display="none"
+            this.pause.style.display="flex"
+        })
+    },
+    
+    pauseMusica(){
+        this.pause.addEventListener('click', () =>{
+            this.musica.pause()
+
+            this.play.style.display="flex"
+            this.pause.style.display="none"
+        })
+    },
+    
+    musicaAnterior(){
+        this.anterior.addEventListener('click', () =>{
+            this.indexMusica--
+            if(this.indexMusica < 0){
+                this.indexMusica = this.data.length - 1
+            }
+            this.renderizarMusica(this.indexMusica)
+            this.musica.play()
+            this.play.style.display="none"
+            this.pause.style.display="flex"
+        })
+    },
+    
+    proximaMusica(){
+        this.proximo.addEventListener('click', () =>{
+            this.indexMusica++
+            if(this.indexMusica > this.data.length - 1){
+                this.indexMusica = 0
+            }
+            this.renderizarMusica(this.indexMusica)
+            
+            this.musica.play()
+            this.play.style.display="none"
+            this.pause.style.display="flex"
+        })
+    },
+    
+// Variavel referente ao tempo da música    
+    barra: document.querySelector('progress'),
+    inicio: document.querySelector('.inicio'),
+    fim: document.querySelector('.fim'),
+    
+    atualizarBarra(){
+
+        let barraAtual = this.barra.style.width = Math.floor(( this.musica.currentTime / this.musica.duration) * 100) + '%'
+        
+        this.inicio.textContent = this.tempo(Math.floor(this.musica.currentTime)) 
+        console.log(typeof barraAtual) 
+    },
+    
+    tempo(segundos){
+        let minutos = Math.floor(segundos / 60)
+        let segundo = segundos % 60
+        if(segundo < 10) {
+            segundo = '0' + segundo
+        }
+        return minutos + ':' + segundo
+    } 
+    
+}
+    
+// Player.renderizarMusica(indexMusica)
+// Player.startMusica()
+Player.playMusica()
+Player.pauseMusica()
+Player.proximaMusica()
+Player.musicaAnterior()
+Player.atualizarBarra()
+Player.tempo()
+Player.restartMusica()
